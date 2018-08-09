880048ee283df8e6c328134e5b4804ba4bf65772
Gustavo Trevisan Dini <gustavotrevisandini@gmail.com>
2018-08-09 02:37:08 -0300

    Cat Clicker Premium With Octopus

--- script/script.js
+++ script/script.js
@@ -1,78 +1,117 @@
-let clickCounter = [0,0,0,0,0];
+/* ======= Model ======= */
 
-let catName = ["ikki","sammy","mia","licota","meg"];
+var model = {
+  selectedCat: 0,
+  catNames: ['ikki','sammy','mia','licota','meg'],
+  cats: [],
+};
 
-let catImages = [];
+/* ======= Octopus ======= */
 
-let selectedCat = 0;
+var octopus = {
 
-let listRows;
+  init: function(){
+    // fetch images from unsplash
+    fetch(`https://api.unsplash.com/search/photos?page=1&query=kitten`, {
+      headers: {
+        Authorization: 'Client-ID 28bef4c545a23c2aebbbb975c8b1d89fbe979f2c0917a7f01e5346b6eb2974d7'
+      }
+    }).then(response => response.json())
+    .then(this.addCats)
+    .then(this.startViews)
+    .catch(e => this.requestError(e, 'image'));
+  },
 
-fetch(`https://api.unsplash.com/search/photos?page=1&query=cat`, {
-  headers: {
-    Authorization: 'Client-ID 28bef4c545a23c2aebbbb975c8b1d89fbe979f2c0917a7f01e5346b6eb2974d7'
+  addCats: function(images){
+    for (let i = 0; i < model.catNames.length; i++){
+      let newCat = {};
+      newCat.name = model.catNames[i];
+      newCat.clickCounter = 0;
+      newCat.image = images.results[i].urls.small;
+      model.cats.push(newCat);
+    }
+  },
+
+  startViews: function(){
+    catListView.init();
+    catView.init();
+  },
+
+  setSelectedCat: function(selection){
+    model.selectedCat = selection;
+    catView.addCatInfo();
+  },
+
+  // increments the counter for the currently-selected cat
+  incrementCounter: function() {
+    model.cats[model.selectedCat].clickCounter++;
+    catView.addCatInfo();
+    catListView.incrementList();
+  },
+
+  requestError: function (e, part) {
+    console.log(e);
   }
-}).then(response => response.json())
-.then(addCats)
-.then(addCatInfo)
-.catch(e => requestError(e, 'image'));
+};
 
-function addCats(images){
-  for (let i = 0; i < catName.length; i++){
-    catImages.push(images.results[i].urls.small);
-  }
-}
 
-function addCatInfo(){
-  $("#cat_images").empty();
-  let htmlImageInfo =
-  `<p class="text" id="cat_name">${catName[selectedCat]}</p>
-  <figure>
-    <img id="cat_image" src="${catImages[selectedCat]}" alt="Cat Image" height="600" width="450">
+/* ======= View ======= */
+
+var catView = {
+
+  init: function() {
+    $("#cat_images").click(function(){
+      octopus.incrementCounter();
+    });
+    // render this view (update the DOM elements with the right values)
+    this.addCatInfo();
+  },
+
+  addCatInfo: function(){
+    $("#cat_images").empty();
+    let htmlImageInfo =
+    `<p class="text" id="cat_name">${model.cats[model.selectedCat].name}</p>
+    <figure>
+    <img id="cat_image" src="${model.cats[model.selectedCat].image}" alt="Cat Image" height="600" width="450">
     <figcaption >
-      <p>Cliques</p>
-      <p id="click_number">${clickCounter[selectedCat]}</p></figcaption>
-  </figure>`
+    <p>Cliques</p>
+    <p id="click_number">${model.cats[model.selectedCat].clickCounter}</p></figcaption>
+    </figure>`
 
-  $("#cat_images").html(htmlImageInfo);
+    $("#cat_images").html(htmlImageInfo);
+  }
+};
 
-  addCatList();
-}
+var catListView = {
 
-function addCatList(){
-  $("#list").empty();
-  let htmlListInfo =
-  `<tr>
+  init: function() {
+    $("#list").empty();
+    let htmlListInfo =
+    `<tr>
     <th>Gato</th>
     <th>Cliques</th>
-  </tr>`
-  $("#list").append(htmlListInfo);
-  for (let i = 0; i < catName.length; i++){
-    htmlListInfo =
-    `<tr>
-      <td>${catName[i]}</td>
-      <td class="counter">${clickCounter[i]}</td>
     </tr>`
     $("#list").append(htmlListInfo);
+    for (let i = 0; i < model.cats.length; i++){
+      htmlListInfo =
+      `<tr id="cat-list${i}">
+      <td>${model.cats[i].name}</td>
+      <td class="counter cat${i}">${model.cats[i].clickCounter}</td>
+      </tr>`
+      $("#list").append(htmlListInfo);
+      $("#cat-list" + i).bind('click', function(catCopy) {
+        return function() {
+          octopus.setSelectedCat(catCopy);
+        };
+      }(i));
+    }
+  },
+
+  incrementList: function(){
+    $(".cat" + model.selectedCat).text(model.cats[model.selectedCat].clickCounter);
   }
-}
 
-function requestError(e, part) {
-  console.log(e);
-}
+};
 
-$('#list').click(function(){
-   $('td').click(function(){
-     var row_index = $(this).parent().index();
-     if (row_index != 0){
-       selectedCat = row_index-1;
-       addCatInfo();
-     };
-   });
-});
-
-$("#cat_images").click(function(){
-  clickCounter[selectedCat] += 1;
-  $("#click_number").html(clickCounter[selectedCat]);
-  addCatList();
-});
+// make it go!
+octopus.init();
